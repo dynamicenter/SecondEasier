82717c6e4ca86b728df8598212adb71d5d9df73e
dynamicenter <dynamicentery@gmail.com>
2017-05-23 18:00:32 +0300

    Update SellerController.java

--- src/main/java/com/seller/SellerController.java
+++ src/main/java/com/seller/SellerController.java
@@ -1,26 +1,23 @@
 package com.seller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @RestController
 @RequestMapping(value = "/seller")
 public class SellerController {
 
 	private final SellerService sellerService;
 	
-	@Autowired
-	public SellerController(SellerController sellerController) {
-		this.sellerController = sellerController;
+	public SellerController(SellerService sellerService) {
+		this.sellerService = sellerService;
 	}
 	
 	@RequestMapping("/user")
-		public Seller getSeller(@RequestParam(value=("Name",defaultValue="Bob")String Name,@RequestParam(value="serName",defaultValue="Marley")String serName) {
+		public Seller getSeller(@RequestParam(value="Name",defaultValue="Bob")String Name,@RequestParam(value="serName",defaultValue="Marley")String serName) {
 			return sellerService.getSeller(Name,serName);
 		}
 }
